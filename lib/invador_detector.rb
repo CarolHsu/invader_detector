34179c3e23819d021222fe4d87936fc18c97f9c1
shesee <sheseee@gmail.com>
2018-01-27 20:35:15 +0800

    Add #anlysis and private method #report

--- lib/invador_detector.rb
+++ lib/invador_detector.rb
@@ -8,6 +8,17 @@ module Detector
       @invador_exists = false
     end
 
+    def analysis
+      @invadors = known_invadors
+      @invadors.each do |invador|
+        @invador_exists = compare_result(invador)
+        break if @invador_exists
+      end
+      
+      report
+    end
+
+
     private
 
     def known_invadors
@@ -18,6 +29,75 @@ module Detector
         invadors.push invador
       end
       invadors
+    end
+
+    def characteristic_value(invador)
+      char = {}
+      invador.each_with_index do |line, location|
+        if line.split("").uniq.size == 1
+          char[line] = location
+        end
+      end
+      char
+    end
+
+    def compare_result(invador)
+      char_info = characteristic_value(invador)
+      char_value = char_info.keys.first
+      char_location = char_info.values.first
+      @on_the_image = []
+      @radar_image.each_with_index do |line, location|
+        left_index = line.index(char_value)
+        next if left_index.nil?
+        right_index = left_index + char_value.size - 1
+
+        # check to upper side
+        0.upto(char_location) do |n|
+          snippet = @radar_image[location - n][left_index..right_index]
+          if snippet == invador[char_location - n]
+            @on_the_image.push [snippet, location - n]
+            next
+          else
+            @on_the_image = []
+            break
+          end
+        end
+
+        # check to lower side
+        0.upto(invador.size - 1 - char_location) do |n|
+          if (location + n) > (@radar_image.size - 1)
+            @on_the_image = []
+            break
+          end
+          snippet = @radar_image[location + n][left_index..right_index]
+          if snippet == invador[char_location + n]
+            @on_the_image.push [snippet, location + n]
+            next
+          else
+            @on_the_image = []
+            break
+          end
+        end
+
+        break unless @on_the_image.empty?
+      end
+
+      @on_the_image.empty? ? false : true
+    end
+
+    def report
+      if @invador_exists
+        puts "Oh no! Invador is existing!"
+        puts "---------------------------"
+        puts image_snippet.inspect
+      else
+        puts "Safe!"
+      end
+    end
+
+    def image_snippet
+      return @on_the_image if @on_the_image.empty?
+      @on_the_image.sort_by(&:last)
     end
   end
 end
--- tests/invador_detector_test.rb
+++ tests/invador_detector_test.rb
@@ -17,5 +17,48 @@ class TestInvador < Minitest::Test
     assert_equal file_of_invadors.size, invadors.size
   end
 
+  def test_get_characteristic_value_from_invador
+    fake_invador = ["--o--", "ooooo", "-o-o-"]
+    cinfo = @detector.send(:characteristic_value, fake_invador)
+    assert_equal "ooooo", cinfo.keys.first
+  end
+
+  def test_report_should_puts_safe_when_there_no_invador
+    @detector.instance_variable_set("@invador_exists", false)
+    assert_output(/Safe!/) { @detector.send(:report) }
+  end
+
+  def test_report_should_puts_oh_no_when_there_is_invador
+    fake_image_info = [
+      ["ooooo", 2],
+      ["--o--", 1],
+      ["-o-o-", 3]
+    ]
+    @detector.instance_variable_set("@on_the_image", fake_image_info)
+    @detector.instance_variable_set("@invador_exists", true)
+    assert_output(/Oh no!/) { @detector.send(:report) }
+  end
+
+  def test_shows_image_snippet_if_not_empty
+    fake_image_info = [
+      ["ooooo", 2],
+      ["--o--", 1],
+      ["-o-o-", 3]
+    ]
+    @detector.instance_variable_set("@on_the_image", fake_image_info)
+    assert_equal "ooooo", @detector.send(:image_snippet)[1][0]
+  end
+
+  def test_reture_true_if_matched
+    fake_invador = @detector.radar_image
+    result = @detector.send(:compare_result, fake_invador)
+    assert_equal true, result
+  end
+
+  def test_reture_true_if_not_matched
+    fake_invador = @detector.radar_image.reverse
+    result = @detector.send(:compare_result, fake_invador)
+    assert_equal false, result
+  end
 
 end
